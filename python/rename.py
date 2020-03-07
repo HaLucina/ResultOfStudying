0cb71d9b828c4db8380498354117364d0aaec04c
HaLucina <58333749+HaLucina@users.noreply.github.com>
2020-03-07 18:57:12 +0900

    Update rename.py

--- python/rename.py
+++ python/rename.py
@@ -4,16 +4,28 @@ from pathlib import Path
 import glob
 import re
 import pprint
-
 import os
 
-tagetName   = "20xx年度報告書.xlsx"
-after       = "2019年度報告書.xlsx"
+tagetName   = '^(?=.*20xx).*$'
+after       = r"2019年度報告書.xlsx"
 p = Path(os.getcwd())
 
-files = sorted(p.glob("**/*"))
+
+
+files = sorted(p.glob("**/*.xls*"))
+repatter = re.compile(tagetName)
+
+
 
 for file_ in files:
-    print(file_)
+    result = repatter.match(str(file_))
 
+    #無駄にif文を入れている理由→http://sookibizviz.blog81.fc2.com/blog-entry-1737.html
+    if result == None:
+        continue
+    else:
+        print(result)
+        #os.path.split(result)
+
+print('===END===')
 input()
