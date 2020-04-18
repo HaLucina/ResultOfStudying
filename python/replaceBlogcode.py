d52bb106abec5275c25cd6b279c5c8df0cda12ba
HaLucina <58333749+HaLucina@users.noreply.github.com>
2020-04-18 19:22:52 +0900

    Update replaceBlogcode.py

--- python/replaceBlogcode.py
+++ python/replaceBlogcode.py
@@ -7,26 +7,36 @@ import pprint
 import os, sys
 import fileinput
 
-suketto = (
+
+# https://qiita.com/hasegit/items/2cf05de74680717f9010
+switch_case = (
     ("c.*", "Brooks Litchfield Conrad"),
     ("k.*", 'Michael Arthur Kinkade'),
-    ("m.*", "Kevin Ford Mench"),
-    (".*",  "Marcos Vechionacci")
+    ("m.*", "Kevin Ford Mench")
 )
 
-def convert_code(files):
-    with fileinput.FileInput(file_name, inplace=True, backup=".bak") as f:
+# https://note.nkmk.me/python-str-replace-translate-re-sub/
+# https://gist.github.com/ginrou/5787895
+# https://gammasoft.jp/blog/text-file-edit-by-python/
+
+
+def convert_code(file_name):
+    with fileinput.FileInput(file_name, openhook=open(file_name,encoding  = "utf-8"), inplace=True, backup=".bak") as f:
         for line in f:
-            dame_gaijin = lambda player: next(v for k,v in suketto if re.match(k,player))  
-            print(line.replace("グーグルマップ", "Google マップ "), end="")
+            replace_code = next(v for k,v in switch_case if re.match(k,line))  
+            print("print = %s" % replace_code) 
+            print(line.replace(line, replace_code), end="")
 
 if __name__ == '__main__':  #このファイルを本体として実行した場合、mainが実行される。
-    # param set
     p   = Path(os.getcwd())
     mds = sorted(p.glob("**/*.md"))
-    gen_iter = (os.path.basename(f) for f in mds)
+    gen_iter = (os.path.abspath(f) for f in mds)    
     print(sys.getsizeof(gen_iter))
 
     for f in gen_iter:
         print(f)
-       # input()
+        try: convert_code(f)
+        except StopIteration: break
+
+
+input()
