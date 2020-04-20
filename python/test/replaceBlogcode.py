7eaf74be2e43d3769207648e167828ba517a6f38
HaLucina <58333749+HaLucina@users.noreply.github.com>
2020-04-21 02:45:27 +0900

    Update replaceBlogcode.py

--- python/test/replaceBlogcode.py
+++ python/test/replaceBlogcode.py
@@ -7,19 +7,33 @@ import pprint
 import os, sys
 import fileinput
 
-def notMatch(args):
+
+def convert_blogCard(args):
+    url = re.search(r'\d+', args)
+    syortCode = "{{< blogcard url=" + url + " >}}"
+    return "blogCard"
+
+def convert_tweet(args):
+    num = re.search(r'\d+', args)
+    syortCode = "{{< tweet " + num + " >}}"
+    return "tweet"
+
+def convert_imgUrl(args):
+    jpg = re.search(r'', args)
+    syortCode = "![test](/img/uploads/" + jpg + ")"
+    return syortCode
+
+def noMatch(args):
     return args
 
-def convert_card(args):
-    return "hoge"
 
 # https://qiita.com/hasegit/items/2cf05de74680717f9010
 case = {
-    ".*blogcard.*" : convert_card,
-    "k.*": 'Michael Arthur Kinkade',
-    "m.*": "Kevin Ford Mench",
-    ".*": notMatch
-}
+    ".*blogcard url.*"                          : convert_blogCard,
+    ".*twitter.com/.*/status/.*"                : convert_tweet,
+    ".*hackheatharu.xyz/wp-content/uploads/.*"  : convert_imgUrl,
+    ".*" : noMatch
+    }
 
 # https://note.nkmk.me/python-str-replace-translate-re-sub/
 # https://gist.github.com/ginrou/5787895
@@ -34,11 +48,12 @@ def convert_code(file_name):
             replace_methoed = next(case[k] for k in case if re.match(k, line))
             contents.append(replace_methoed(line))
 
+    #rewriteData = "\n".join(contents)
     [print(i) for i in contents]
     with open("result.md", mode="w", encoding="utf-8") as f:
-        f.write(contents)
+        f.writelines(contents)
 
-if __name__ == '__main__':  #このファイルを本体として実行した場合、mainが実行される。
+if __name__ == '__main__':  
     p   = Path(os.getcwd())
     mds = sorted(p.glob("**/*.md"))
     gen_iter = (os.path.abspath(f) for f in mds)    
